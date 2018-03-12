5643c3f83d00a22eb693620407e6fa9e7b0aef8d
silastittes <silas.tittes@gmail.com>
2018-03-11 21:15:02 -0600

    fixed bug that multiplied number of requested pomodoros by 60. Added a final noise to signal end of pomodoros.

--- R/pomodoror.R
+++ R/pomodoror.R
@@ -7,6 +7,7 @@
 #' @param break_length Amount of time in minutes to break before getting back to work.
 #' @param work_start Noise to make to signal beginning of work time.
 #' @param work_end Noise to make to signal end of work time/beginning of break time.
+#' @param work_finish Noise to make at after all pomodoros are completed.
 #' @export
 
 pomodoror <- function(
@@ -14,19 +15,25 @@ pomodoror <- function(
   work_length = 0.1,
   break_length = 0.1,
   work_start = "mario",
-  work_end = "complete"
+  work_end = "complete",
+  work_finish = "fanfare"
 ){
-  pomodoros = pomodoros * 60
+  pomodoros = pomodoros
   work_length = work_length * 60
   break_length = break_length * 60
 
   for(i in 1:pomodoros){
-    beep(sound = work_start)
+    beepr::beep(sound = work_start)
     print(stringr::str_glue("round {i}: write for: {round(work_length/60, 1)} minutes"))
     Sys.sleep(work_length)
-    beep(sound = work_end)
-    print(stringr::str_glue("round {i}: break for: {round(break_length/60, 1)} minutes"))
-    Sys.sleep(break_length)
+
+    if(i == pomodoros){
+      beepr::beep(sound = work_finish)
+    } else{
+      beepr::beep(sound = work_end)
+      print(stringr::str_glue("round {i}: break for: {round(break_length/60, 1)} minutes"))
+      Sys.sleep(break_length)
+    }
   }
 }
 
